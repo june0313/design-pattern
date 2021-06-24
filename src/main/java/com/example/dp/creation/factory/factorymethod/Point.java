657d22312bd1d75efd312d3059ed8516e5cfa60e
Young Jun Park <june0313@gmail.com>
2021-06-24 21:27:43 +0900

    Factory - Nested Factory Class

--- src/main/java/com/example/dp/creation/factory/factorymethod/Point.java
+++ src/main/java/com/example/dp/creation/factory/factorymethod/Point.java
@@ -8,18 +8,20 @@ class Point {
         this.y = y;
     }
 
-    public static Point newCartesianPoint(double x, double y) {
-        return new Point(x, y);
-    }
+    public static class Factory {
+        public static Point newCartesianPoint(double x, double y) {
+            return new Point(x, y);
+        }
 
-    public static Point newPolarPoint(double rho, double theta) {
-        return new Point(rho * Math.cos(theta), rho * Math.sin(theta));
+        public static Point newPolarPoint(double rho, double theta) {
+            return new Point(rho * Math.cos(theta), rho * Math.sin(theta));
+        }
     }
 }
 
 class Demo {
     public static void main(String[] args) {
-        Point polarPoint = Point.newPolarPoint(2, 3);
-        Point cartesianPoint = Point.newCartesianPoint(2, 3);
+        Point polarPoint = Point.Factory.newPolarPoint(2, 3);
+        Point cartesianPoint = Point.Factory.newCartesianPoint(2, 3);
     }
 }

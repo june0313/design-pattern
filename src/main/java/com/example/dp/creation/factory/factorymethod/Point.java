8c8ee80efacbf116e54bf0ef47ca31addb89e815
Young Jun Park <june0313@gmail.com>
2021-06-24 21:22:05 +0900

    Factory - Factory Method

---
+++ src/main/java/com/example/dp/creation/factory/factorymethod/Point.java
@@ -0,0 +1,25 @@
+package com.example.dp.creation.factory.factorymethod;
+
+class Point {
+    private double x, y;
+
+    private Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static Point newCartesianPoint(double x, double y) {
+        return new Point(x, y);
+    }
+
+    public static Point newPolarPoint(double rho, double theta) {
+        return new Point(rho * Math.cos(theta), rho * Math.sin(theta));
+    }
+}
+
+class Demo {
+    public static void main(String[] args) {
+        Point polarPoint = Point.newPolarPoint(2, 3);
+        Point cartesianPoint = Point.newCartesianPoint(2, 3);
+    }
+}
